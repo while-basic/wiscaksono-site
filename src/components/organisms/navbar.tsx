7a1a89687f7ded5b716669ce9827e80c64c0d0e6
while-basic <c_celaya@icloud.com>
2024-03-07 01:54:25 -0700

    styling, github url

--- src/components/organisms/navbar.tsx
+++ src/components/organisms/navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
   return (
     <nav className='md:grid grid-cols-12 border-b flex items-center justify-between relative z-10 bg-background overflow-x-auto'>
       <Link href='/' className='md:border-r md:px-5 px-2.5 py-4 text-foreground md:col-span-3 lg:col-span-2 shrink-0 transition-colors'>
-        Chris Celaya
+        Christopher Celaya
       </Link>
       <div className='md:col-span-9 lg:col-span-10 flex items-center justify-between'>
         <ul className='md:flex items-center divide-x w-max border-r hidden shrink-0'>
