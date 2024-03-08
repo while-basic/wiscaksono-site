e9c1ab25f06f721a979a95ec77a6b3f9ba32efdb
while-basic <c_celaya@icloud.com>
2024-03-07 21:45:57 -0700

    added new links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <section className='flex items-center justify-center gap-20 p-5'>
       <FadeIn>
         <div className='md:space-y-10 space-y-8 relative z-10'>
+          
           <header>
             <p className='text-muted-foreground text-lg font-extralight'>Hello all, I am</p>
             <AnimatedName />
@@ -19,7 +20,7 @@ export default function Home() {
           </header>
 
           <div className='space-y-2 sm:text-sm text-xs'>
-            <p className='text-muted-foreground'>{`// Check out my Github page`}</p>
+            <p className='text-muted-foreground'>{`// Github`}</p>
             <p className='text-muted-foreground'>
               <span className='text-purple'>const</span> <span className='text-green'>githubLink</span> ={' '}
               <a target='_blank' href='https://github.com/while-basic' className='text-light-brown hover:underline hover:text-foreground transition-colors'>
@@ -27,6 +28,37 @@ export default function Home() {
               </a>
             </p>
           </div>
+
+          <div className='space-y-2 sm:text-sm text-xs'>
+            <p className='text-muted-foreground'>{`// Instagram`}</p>
+            <p className='text-muted-foreground'>
+              <span className='text-purple'>const</span> <span className='text-green'>githubLink</span> ={' '}
+              <a target='_blank' href='https://instagram.com/chriscelaya' className='text-light-brown hover:underline hover:text-foreground transition-colors'>
+                &apos;https://instagram.com/chriscelaya&apos;;
+              </a>
+            </p>
+          </div>
+
+          <div className='space-y-2 sm:text-sm text-xs'>
+            <p className='text-muted-foreground'>{`// YouTube`}</p>
+            <p className='text-muted-foreground'>
+              <span className='text-purple'>const</span> <span className='text-green'>githubLink</span> ={' '}
+              <a target='_blank' href='https://www.youtube.com/@christophercelaya/videos' className='text-light-brown hover:underline hover:text-foreground transition-colors'>
+                &apos;https://www.youtube.com/@christophercelaya/videos&apos;;
+              </a>
+            </p>
+          </div>
+
+          <div className='space-y-2 sm:text-sm text-xs'>
+            <p className='text-muted-foreground'>{`// Linkedin`}</p>
+            <p className='text-muted-foreground'>
+              <span className='text-purple'>const</span> <span className='text-green'>githubLink</span> ={' '}
+              <a target='_blank' href='https://www.linkedin.com/in/christophercelaya/' className='text-light-brown hover:underline hover:text-foreground transition-colors'>
+                &apos;https://www.linkedin.com/in/christophercelaya/&apos;;
+              </a>
+            </p>
+          </div>
+
         </div>
       </FadeIn>
       <GridPattern
